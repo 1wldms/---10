3297a2db9476238c8dea231a79c5e60d93d0b5c7
1wldms <jieunin0311@gmail.com>
2025-05-22 10:42:59 +0900

    naver 연동...

--- pages/naver_map.py
+++ pages/naver_map.py
@@ -5,25 +5,10 @@ st.sidebar.markdown('# 민원')
 
 from streamlit.components.v1 import html
 
-# 네이버 클라이언트 아이디
-NAVER_CLIENT_ID = "qzkec0z7y6"
+html_file_path = "static/map.html"
 
-map_html = f"""
-<div id="map" style="width:100%;height:400px;"></div>
-<script src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId={NAVER_CLIENT_ID}"></script>
-<script>
-var map = new naver.maps.Map('map', {{
-    center: new naver.maps.LatLng(37.5636201943343, 126.93774785651566), // 연대 좌표
-    zoom: 15
-}});
-
-var marker = new naver.maps.Marker({{
-    map: map,
-    position: map.getCenter()
-}})
-
-</script>
-"""
-
-st.markdown("### 네이버 지도 클릭해서 좌표 확인")
-html(map_html, height=450)
+# iframe으로 불러오기
+st.markdown(
+    f'<iframe src="{html_file_path}" width="100%" height="600" frameborder="0"></iframe>',
+    unsafe_allow_html=True
+)
